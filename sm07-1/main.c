39ad3c83ee8540282c99c674fc8158a96d20c29b
Alex B <alexandr.belonozhkin@gmail.com>
2020-01-12 22:18:52 +0300

    Первая лаба

--- sm07-1/main.c
+++ sm07-1/main.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
+#include <ctype.h>
+
+//https://stackoverflow.com/questions/656542/trim-a-string-in-c
+char *ltrim(char *s)
+{
+    while(isspace(*s)) s++;
+    return s;
+}
+
+char *rtrim(char *s)
+{
+    char* back = s + strlen(s);
+    while(back>s && isspace(*--back));
+    *(back+1) = '\0';
+    return s;
+}
+
+char *trim(char *s)
+{
+    return rtrim(ltrim(s)); 
+}
 
 typedef double (*funcptr_t)(double);
 
 typedef struct FunctionTable_ {
     char name[20];
-    funcptr_t fptr;
+    funcptr_t func;
 } FunctionTable;
 
 FunctionTable fnt[]={	{"sin",&sin},
@@ -19,22 +41,50 @@ FunctionTable fnt[]={	{"sin",&sin},
 
 int main()
 {
-    char str[80];
-    unsigned long n;
+    char str[80], sname[20], sval[20];
+    unsigned long n, k;
+    char *cp;
+    double arg, val;
+    n=sizeof(fnt)/sizeof(FunctionTable);
     while (!feof(stdin))
     {
         if (fgets(str, 80, stdin) == NULL)
         {
             break;
         }
-        printf("%s", str);
+        rtrim(str);
+        if(strlen(str) >0)
+        {
+          printf("%s: ", str);
+          cp = strtok(str, " ");
+          if(cp==NULL) {
+            continue;
+            }
+          strcpy(sname,cp);
+          cp = strtok(NULL, " ");
+          if(cp==NULL) {
+            continue;
+            }
+          strcpy(sval,cp);
+          sscanf(sval,"%lf",&arg);
+          val=NAN;
+          for(k=0; k<n;k++) {
+            if(strcmp(sname,fnt[k].name)==0) {
+              val=fnt[k].func(arg);
+              break;
+            }
+          }
+          printf("%s(%s)==%s(%lf)=%lf\n", sname, sval, sname, arg, val);
+        }
     }
     //sscanf;
-
-    printf("%e\n",fnt[0].fptr(0.5));
-    printf("%e\n",sin(0.5));
-    n=sizeof(fnt)/sizeof(FunctionTable);
-    printf("%lu\n",n);
-
+/*
+//https://stackoverflow.com/questions/9210528/split-string-with-delimiters-in-c
+  ch = strtok(st, " ");
+  while (ch != NULL) {
+  printf("%s\n", ch);
+  ch = strtok(NULL, " ,");
+  }
+*/
     return 0;
 }
--- sm07-1/main.sh
+++ sm07-1/main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo 123
-gcc -lm -o main main.c
+echo ----
+#gcc -lm -o main main.c
 ./main <main.dat
-echo 456
+echo ----
 
