4781b47710407351ae6aec7cb0095aa16745a1d9
Alex B <alexandr.belonozhkin@gmail.com>
2020-02-07 10:08:59 +0300

    sm09-1: Андрей

---
+++ sm09-1/fib_32_ans.s
@@ -0,0 +1,31 @@
+	.code32
+
+	.data
+output_format:
+	.string "%d %d %d\n"
+
+	.text
+        .global print_fib
+	.type	print_fib, @function
+
+print_fib:
+        pushl   %ebp
+
+        movl    %esp, %ebp
+        movl    (A), %eax
+        addl    (B), %eax
+        movl    %eax, %ecx
+        addl    (B), %ecx
+        movl    %ecx, %edx
+        addl    %eax, %edx
+
+        pushl   %edx
+        pushl   %ecx
+        pushl   %eax
+        pushl   $output_format
+        call    printf
+        addl    $4*4, %esp
+
+        popl    %ebp
+        xorl    %eax, %eax
+        ret
