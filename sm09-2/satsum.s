821d8948695af7d7845f6f9940c1d8c98ed2c733
Alex B <alexandr.belonozhkin@gmail.com>
2020-02-08 19:08:45 +0300

    sm09-2: frame + .equ

--- sm09-2/satsum.s
+++ sm09-2/satsum.s
@@ -4,12 +4,17 @@
 	.globl	satsum
 	.type	satsum, @function
 
+	.equ	A1, 4*2
+	.equ	A2, 4*3
+
 //unsigned satsum(unsigned a1, unsigned a2);
 satsum:
 	push	%ebp
 	mov	%esp, %ebp
 
-	xor	%eax, %eax
+	mov	A1(%ebp), %eax
+	add	A2(%ebp), %eax
+//	xor	%eax, %eax
 
 	mov	%ebp, %esp
 	pop	%ebp
