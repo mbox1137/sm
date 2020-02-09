be640c9374367bd377d49839cc9e621ede734a34
Alex B <alexandr.belonozhkin@gmail.com>
2020-02-09 19:51:52 +0300

    sm09-3: setnz работает!!!

--- sm09-3/mystrcmp.s
+++ sm09-3/mystrcmp.s
@@ -7,7 +7,7 @@
 	.equ	str1, 4*2
 	.equ	str2, 4*3
 
-	.equ	MAXLEN, 1000
+	.equ	MAXLEN, -1
 
 //int mystrcmp(const char *str1, const char *str2);
 mystrcmp:
@@ -20,6 +20,7 @@ mystrcmp:
 
 	xor	%eax, %eax
 
+//длина 1-й строки
 	mov	$MAXLEN, %ecx
 	mov	str1(%ebp), %edi
 repnz	scasb
@@ -28,25 +29,30 @@ repnz	scasb
 
 	mov	%ecx, %edx
 
+//длина 2-й строки
 	mov	$MAXLEN, %ecx
 	mov	str2(%ebp), %edi
 repnz	scasb
 	sub	$MAXLEN, %ecx
 	neg	%ecx
 
+//наименьшая длина
 	cmp	%ecx, %edx
 	jge	m2
 	mov	%edx, %ecx
 m2:
 
+//поиск несовпадения, но не больше длины наименьшей строки
 	mov	str1(%ebp), %esi
 	mov	str2(%ebp), %edi
 repz	cmpsb
 
-	jz	m1
-	mov	$1, %eax
-	jg	m1
+//https://c9x.me/x86/html/file_module_x86_id_288.html
+	setnz	%al
+	jge	m3
 	neg	%eax
+m3:
+
 m1:
 	pop	%edi
 	pop	%esi
