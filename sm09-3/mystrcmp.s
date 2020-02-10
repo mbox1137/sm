e7516963bb786a0cf9654f84f69f84ca88a8e982
Alex B <alexandr.belonozhkin@gmail.com>
2020-02-10 18:39:26 +0300

    sm09-3: len(str2) only!!!

--- sm09-3/mystrcmp.s
+++ sm09-3/mystrcmp.s
@@ -19,43 +19,22 @@ mystrcmp:
 	push	%edi
 
 	cld	/* ++ */
-
 	xor	%eax, %eax
 
-//длина 1-й строки
-	mov	$MAXLEN, %ecx
-	mov	str1(%ebp), %edi
-repnz	scasb
-	sub	$MAXLEN, %ecx
-	neg	%ecx
-
-	mov	%ecx, %edx
-
-//длина 2-й строки
-	mov	$MAXLEN, %ecx
 	mov	str2(%ebp), %edi
+	mov	$MAXLEN, %ecx
 repnz	scasb
 	sub	$MAXLEN, %ecx
 	neg	%ecx
 
-//наименьшая длина
-	cmp	%ecx, %edx
-	jge	m2
-	mov	%edx, %ecx
-m2:
-
-//поиск несовпадения, но не больше длины наименьшей строки
 	mov	str1(%ebp), %esi
 	mov	str2(%ebp), %edi
 repz	cmpsb
-
 //https://c9x.me/x86/html/file_module_x86_id_288.html
 	setnz	%al
 	jge	m3
 	neg	%eax
 m3:
-
-m1:
 	pop	%edi
 	pop	%esi
 	mov	%ebp, %esp
