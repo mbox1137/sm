c897aa192b901f42b8e16f83bd5cbb1f0cf23b1f
Alex B <alexandr.belonozhkin@gmail.com>
2020-03-01 20:51:13 +0300

    sm11-2: Андрей

--- sm11-2/main.c
+++ sm11-2/main.c
@@ -1,62 +1,74 @@
 #include <stdio.h>
 
-int mygetchar(void);
+int mygetchar(void)
+{
+    int ic=0;
+    asm(
+    "	mov	$3, %%eax	\n\t"
+    "	mov	$0, %%ebx	\n\t"
+    "	lea	%1, %%ecx	\n\t"
+    "	mov	$1, %%edx	\n\t"
+    "	int	$0x80		\n\t"
+    "	cmp	$1, %%eax	\n\t"
+    "	je	mm1		\n\t"
+    "	xor	%%eax, %%eax	\n\t"
+    "	dec	%%eax		\n\t"
+    "	mov	%%eax, %0	\n\t"
+    "mm1:			"
+        :"=m"(ic)
+        :"m"(ic)
+        :"eax","ebx","ecx","edx");
 
-int mygetchar(void) {
-    int ic=0, res=0;
-asm(
-"	mov	$3, %%eax	\n\t"
-"	mov	$0, %%ebx	\n\t"
-"	lea	%1, %%ecx	\n\t"
-"	mov	$1, %%edx	\n\t"
-"	int	$0x80		\n\t"
-"	mov	%%eax, %0	\n\t"
-    :"=m"(res)
-    :"m"(ic)
-    :"eax","ebx","ecx","edx");
-    return(res==1?ic:-1);
+    return(ic);
 }
 
-void myputchar(int ic) {
-asm(
-"	mov	$4, %%eax	\n\t"
-"	mov	$1, %%ebx	\n\t"
-"	lea	%1, %%ecx	\n\t"
-"	mov	$1, %%edx	\n\t"
-"	int	$0x80		    "
-    :"=m"(ic)
-    :"m"(ic)
-    :"eax","ebx","ecx","edx");
+void myputchar(int ic)
+{
+    asm(
+    "	mov	$4, %%eax	\n\t"
+    "	mov	$1, %%ebx	\n\t"
+    "	lea	%1, %%ecx	\n\t"
+    "	mov	$1, %%edx	\n\t"
+    "	int	$0x80		    "
+        :"=m"(ic)
+        :"m"(ic)
+        :"eax","ebx","ecx","edx");
+
     return;
 }
 
-void myexit(int ret_val) {
-asm(
-"	movl	$1, %%eax	\n\t"
-"	movl	%1, %%ebx	\n\t"
-"	int	$0x80		    "
-    :"=m"(ret_val)
-    :"m"(ret_val)
-    :"eax","ebx");
+void myexit(int ret_val)
+{
+    asm(
+    "	movl	$1, %%eax	\n\t"
+    "	movl	%1, %%ebx	\n\t"
+    "	int	$0x80		    "
+        :"=m"(ret_val)
+        :"m"(ret_val)
+        :"eax","ebx");
+
     return;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     int ic;
     unsigned char cm = 'A' ^ 'a';
-    while(1) {
+    while(1)
+    {
         ic=mygetchar();
-        if(ic==EOF) {
+        if(ic==EOF)
             break;
-        }
         if(ic>='a' && ic<='z')
             ic&=~cm;
         myputchar(ic);
     }
+
     return(0);
 }
 
-void _start(void) {
+void _start(void)
+{
     int retval;
     retval=main(0,0);
     myexit(retval);
