5dc3d8299930c749d6f4a660f7146ea746001813
Alex B <alexandr.belonozhkin@gmail.com>
2020-03-04 12:58:33 +0300

    sm11-4: diff -> cmp

--- sm11-4/main.sh
+++ sm11-4/main.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
 function myrun {
-	./fdat.py "$1" >in.dat
+	if [ -f "$1" ]; then
+		cp "$1" in.dat
+	else
+		./fdat.py "$1" >in.dat
+	fi
 	./main <in.dat >out.dat
-	diff in.dat out.dat
-	echo "$(if [ $?==0 ] ;then echo Ok ;else echo Fail ;fi): $(wc -c in.dat) $(wc -c out.dat)"
+	echo "$1 $(if cmp -s in.dat out.dat; then echo Ok ;else echo Fail ;fi): $(wc -c in.dat) $(wc -c out.dat)"
 }
 
-myrun "0"
-myrun "5"
-myrun "4093"
-myrun "4094"
-myrun "4095"
-myrun "4096"
-myrun "10000000"
+if [ -x "./main" ]; then
+	myrun "0"
+	myrun "5"
+	myrun "4093"
+	myrun "4094"
+	myrun "4095"
+	myrun "4096"
+	myrun "10000000"
+	myrun "null.dat"
+else
+	echo "run make..."
+fi
