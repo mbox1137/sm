da392a70055b49e1cfed92b6777653391e7b5182
Alex B <alexandr.belonozhkin@gmail.com>
2020-03-14 11:01:12 +0300

    sm12-3: _

--- sm12-3/main.c
+++ sm12-3/main.c
@@ -7,59 +7,29 @@
 #include <fcntl.h>
 #include <string.h>
 #include <malloc.h>
-#include "binary-tree.h"
+#include "read_file.h"
 
-const static int m=sizeof(struct Node);
-static int nkeys;
-static int *keys;
-static int h;
-
-int scan_tree(int nd);
-
-int scan_tree(int nd) {
-    int retcode;
-    struct Node node;
-    retcode=0;
-    if(lseek(h,nd*m,SEEK_SET)!=nd*m || read(h, &node, m)!=m) {
-        retcode=1;
-    } else {
-        if(node.left_idx !=0) retcode|=scan_tree(node.left_idx);
-        if(node.right_idx!=0) retcode|=scan_tree(node.right_idx);
-        keys[nkeys++]=node.key;
-    }
-    return(retcode);
-}
-
-static int cmpints(const void *p1, const void *p2) {
-    return (*(int*)p2)-(*(int*)p1);
-}
+const static int m=sizeof(struct FileContent);
 
 int main(int argc, char **argv)
 {
+    int h,h2;
     int k, fsize;
-    struct stat st;
-    char fn[80]="main.dat";
-    if(argc==2) {
+    char fn[80]="main.tst";
+    char fn2[80];
+    struct FileContent fc={0, NULL};
+    if(argc==3) {
         sscanf(argv[1],"%s",fn);
+        sscanf(argv[1],"%s",fn2);
     }
-    nkeys=0;
-    stat(fn, &st);
-    fsize = st.st_size;
-    nkeys=fsize/m;
     h=open(fn, O_RDONLY);
-    keys=(int*) calloc(nkeys,sizeof(int));
-    if(h!=0 && keys!=0) {
-        nkeys=0;
-        if(scan_tree(0)) {
-            exit(-2);
-        } else {
-            qsort(keys, nkeys, sizeof(int), cmpints);
-            for(k=0; k<nkeys; k++) printf("%d ",keys[k]); printf("\n");
-        }
-    } else {
-        exit(-1);
+    h2=creat(fn2, 0644);
+    if(h!=0 && h2!=0) {
+        fc=read_file(h);
+        write(h2, fc.data, fc.size);
     }
-    free(keys);
+    free(fc.data);
     close(h);
+    close(h2);
     return(0);
 }
