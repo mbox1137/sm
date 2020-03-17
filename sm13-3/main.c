cc34351c6935245edd5903f9e854272989eb88ce
Alex B <alexandr.belonozhkin@gmail.com>
2020-03-17 14:02:56 +0300

    sm13-3: _

--- sm13-3/main.c
+++ sm13-3/main.c
@@ -1,12 +1,54 @@
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+#define NN 1000
 
 int main(int argc, char **argv)
 {
     char fn[80]="main.dat";
+    FILE *f;
+    char str[NN];
+    char delim[]=" \t\r\n";
+    char *dat, *tim;
+    int y, mon, d, h, m, s;
+    time_t t0, t, dt;
+    struct tm tm;
     if(argc==2)
-    {
         sscanf(argv[1],"%s",fn);
-    } else
+    else
         printf("./main [main.dat]\n");
+    t0=0;
+    f=fopen(fn,"r");
+    while(!feof(f)) {
+        if (fgets(str, NN, f) == NULL)
+            break;
+        printf("Source:\t%s",str);
+        dat = strtok(str, delim);
+        tim = strtok(NULL, delim);
+        printf("Clean:\t%s %s\n", dat, tim);
+        sscanf(dat,"%d/%d/%d",&y,&mon,&d);
+        sscanf(tim,"%d:%d:%d",&h,&m,&s);
+        printf("Ints:\t%d %d %d %d %d %d\n", y, mon, d, h, m, s);
+        tm.tm_sec	= s;		// Seconds (0-60) 
+        tm.tm_min	= m;		// Minutes (0-59) 
+        tm.tm_hour	= h-1;		// Hours (0-23) 
+        tm.tm_mday	= d-1;		// Day of the month (1-31) 
+        tm.tm_mon	= mon-1;		// Month (0-11) 
+        tm.tm_year	= y-1900;	// Year - 1900 
+/*
+        tm.tm_wday;			// Day of the week (0-6, Sunday = 0) 
+        tm.tm_yday;			// Day in the year (0-365, 1 Jan = 0) 
+        tm.tm_isdst;		// Daylight saving time 
+*/
+        t=mktime(&tm);
+        printf("%s\n", ctime(&t));
+        if(t0!=0)
+        {
+        }
+        t0=t;
+        printf("\n");
+    }
+    fclose(f);
     return(0);
 }
--- sm13-3/main.sh
+++ sm13-3/main.sh
@@ -2,22 +2,7 @@
 
 if [ -x ./main ]
 then
-	./main 10 0
-	./main 3 0777
-	./main 10 0777
-	./main 10 0666
-	echo SUID
-	./main 10 04000
-	./main 10 04010
-	./main 10 04001
-	echo SGID
-	./main 10 02000
-	./main 10 02001
-	echo Sticky
-	./main 10 01000
-	./main 10 01002
-	./main 10 01001
-	./main 10 01003
+	./main main.dat
 else
 	echo "run make"
 fi
