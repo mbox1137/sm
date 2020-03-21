f7716d9e7db7bb1efc8b0040b29ea6422e3bf9c8
Alex B <alexandr.belonozhkin@gmail.com>
2020-03-22 00:00:17 +0300

    ku04-2: !

--- ku04-2/main.c
+++ ku04-2/main.c
@@ -2,8 +2,24 @@
 #include <stdint.h>
 #include "process.h"
 
-int main() {
+int main(int argc, char **argv) {
+    uint32_t res;
     struct S s;
-    process(&s);
+    if(argc == 5) {
+     sscanf(argv[1],"%d",&s.z[0]);
+     sscanf(argv[2],"%d",&s.z[1]);
+     sscanf(argv[3],"%d",&s.y[0]);
+     sscanf(argv[4],"%d",&s.y[1]);
+    } else {
+     s.z[0]=3;
+     s.z[1]=4;
+     s.y[0]=5;
+     s.y[1]=6;
+    }
+    res=process(&s);
+    printf("%d*%d+%d*%d=%lld(%d)\n", 
+               s.z[0],s.y[1] , s.z[1],s.y[0],
+     (uint64_t)s.z[0]*s.y[1] + s.z[1]*s.y[0],
+               res);
     return 0;
 }
---
+++ ku04-2/main.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+./main
+./main 1 2 3 4
