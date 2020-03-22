aa96cdfa668174f636bbcf5ed2ba9d3e0b27bc41
Alex B <alexandr.belonozhkin@gmail.com>
2020-03-22 22:55:32 +0300

    ku04-2: !

--- ku04-2/main.c
+++ ku04-2/main.c
@@ -3,26 +3,31 @@
 #include "process.h"
 
 int main(int argc, char **argv) {
-    uint32_t res;
+    int32_t res;
     struct S s;
+    int hn;
     if(argc == 5) {
      sscanf(argv[1],"%d",&s.z[0]);
      sscanf(argv[2],"%d",&s.z[1]);
      sscanf(argv[3],"%d",&s.y[0]);
      sscanf(argv[4],"%d",&s.y[1]);
     } else {
-     s.z[0]=3;
-     s.z[1]=4;
-     s.y[0]=5;
-     s.y[1]=6;
+     s.z[0]=2147483647;
+     s.z[1]=2147483647;
+     s.y[0]=8;
+     s.y[1]=9;
     }
     res=process(&s);
 
 //long long t = (long long) p->z[0] * (long long) p->y[1] + (long long) p->z[1] * (long long) p->y[0];
 
-    printf("%d*%d+%d*%d=%lld(%d)\n",
-               s.z[0],s.y[1] , s.z[1],s.y[0],
-     ((int64_t)s.z[0]*s.y[1] + s.z[1]*s.y[0]) / 16,
-               res);
+    hn=16;
+    printf("(%d*%d+%d*%d)/%d=%lld",
+       s.z[0],s.y[1] , s.z[1],s.y[0], hn,
+       ((int64_t)s.z[0] * (int64_t)s.y[1] + 
+       (int64_t)s.z[1] * (int64_t)s.y[0]) / 
+       (int64_t)hn );
+    printf("(%u:%d)",res,res);
+    printf("\n");
     return 0;
 }
--- ku04-2/main.sh
+++ ku04-2/main.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+#https://defuse.ca/big-number-calculator.htm
 ./main
 ./main 1 2 3 4
 ./main 16 16 2 3
 ./main -1 1 -1 1
 ./main -10 1 -5 4
 ./main -10 5 4 5
+echo 671 088 639:
 ./main 2147483647 2147483647 2 3
+echo 2 281 701 374:
 ./main 2147483647 2147483647 8 9
+./main 2147483647 2147483647 16 18
+./main 2147483647 2147483647 32 36
