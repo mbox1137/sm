3d92f91691d286f3c30e1c7aac3fd1c224abbe8d
Alex B <alexandr.belonozhkin@gmail.com>
2020-04-22 01:44:57 +0300

    sm14-3: w/o qsort_r_

--- sm14-3/main.c
+++ sm14-3/main.c
@@ -1,4 +1,5 @@
-#define DEBUG 0
+#define DEBUG 1
+#define _GNU_SOURCE 1
 
 #include <errno.h>
 #include <stdio.h>
@@ -11,7 +12,7 @@
 
 typedef struct {
     char *names;
-    char **pnames;
+    off_t *pnames;
     off_t kn;
     size_t nn;
     off_t kpn;
@@ -39,30 +40,34 @@ void addname(CTRLN *z, char *name) {
     }
     if((z->kn+n+1) >= (z->nn)) {
         if(!z->names) {
-            z->nn = 1<<10;
+            z->nn = 1<<12;
             z->names = malloc(z->nn);
+            if(z->names == NULL)
+                z->names = NULL;
         } else {
             z->nn *= 2;
             z->names = realloc(z->names, z->nn);
+            if(z->names == NULL)
+                z->names = NULL;
         }
     }
     if((z->kpn) >= (z->npn)) {
         if(!z->pnames) {
-            z->npn = 1<<7;
-            z->pnames = malloc(z->npn*sizeof(char*));
+            z->npn = 1<<9;
+            z->pnames = malloc(z->npn*sizeof(off_t));
+            if(z->pnames == NULL)
+                z->pnames = NULL;
         } else {
             z->npn *= 2;
-            z->pnames = realloc(z->pnames, z->npn*sizeof(char*));
+            z->pnames = realloc(z->pnames, z->npn*sizeof(off_t));
+            if(z->pnames == NULL)
+                z->pnames = NULL;
         }
     }
-    (z->pnames)[(z->kpn)++]=&((z->names)[z->kn]);
+    (z->pnames)[z->kpn]=z->kn;
     strcpy(&((z->names)[z->kn]),name);
     z->kn += n+1;
-}
-
-static int mycmp(const void *p1, const void *p2)
-{
-    return strcasecmp(* (char * const *) p1, * (char * const *) p2);
+    z->kpn++;
 }
 
 char *lastname(char *path)
@@ -125,6 +130,11 @@ int filelist(char **names, char *path)
     return nf;
 }
 
+static int mycmp_(const void *p1, const void *p2)
+{
+    return strcasecmp(* (char * const *) p1, * (char * const *) p2);
+}
+
 void traverse_(char *path, char *name)
 {
     char** pnames;
@@ -164,7 +174,7 @@ void traverse_(char *path, char *name)
         if (pnames == NULL)
             exit(1);
         filelist(pnames, path);
-        qsort(pnames, nf, sizeof(char *), mycmp);
+        qsort(pnames, nf, sizeof(char *), mycmp_);
 
         for (k = 0; k < nf; k++)
         {
@@ -186,13 +196,71 @@ void traverse_(char *path, char *name)
     return;
 }
 
+static int mycmp(off_t *k1, off_t *k2, void *pz)
+{
+    char *p1, *p2;
+    CTRLN *z;
+    z=(CTRLN*) pz;
+    p1=&(z->names[z->pnames[*k1]]);
+    p2=&(z->names[z->pnames[*k2]]);
+    return strcasecmp(p1, p2);
+}
+
 void traverse(char *path, char *name)
 {
     CTRLN ctrln;
     ctrln.names=NULL;
     ctrln.pnames=NULL;
+    struct stat statbuf;
+    char ffn[PATH_MAX];
+    char tmpp[PATH_MAX];
+    off_t k;
+    DIR *dir;
+    struct dirent *de;
+
     addname(&ctrln, NULL);	//init
+
+    strcpy(tmpp, path);
+#if DEBUG
+    printf("cd %s\n", tmpp);
+#else
+    if (name)
+        printf("cd %s\n", lastname(tmpp));
+#endif
+    if ((dir = opendir(path)) == NULL)
+        {
+            strerror_r(errno, tmpp, PATH_MAX);
+            fprintf(stderr, "Err: opendir (%d=%s) %s\n", errno, tmpp, path);
+            return;
+        }
+    while((de=readdir(dir)) != NULL)
+    {
+        sprintf(ffn, "%s/%s", path, de->d_name);
+
+        if (lstat(ffn, &statbuf) != 0)
+            continue;
+        if (!S_ISDIR(statbuf.st_mode))
+            continue;
+        if (strcmp(de->d_name,".") == 0)
+            continue;
+        if (strcmp(de->d_name,"..") == 0)
+            continue;
+        addname(&ctrln, de->d_name);
+    }
+    closedir(dir);
+//    qsort_r(ctrln.pnames, ctrln.npn, sizeof(off_t), mycmp, &ctrln);
+
+    for (k = 0; k < ctrln.kpn; k++)
+    {
+        sprintf(ffn, "%s/%s", path, &ctrln.names[ctrln.pnames[k]]);
+        traverse(ffn, &ctrln.names[ctrln.pnames[k]]);
+    }
     addname(&ctrln, NULL);	//finit
+#if DEBUG
+#else
+    if(name)
+        printf("cd ..\n");
+#endif
 }
 
 int main(int argc, char *argv[])
@@ -205,6 +273,6 @@ int main(int argc, char *argv[])
         fprintf(stderr, "%s [dir]\n",argv[0]);
         return -1;
     }
-    traverse_(fn, NULL);
+    traverse(fn, NULL);
     return 0;
 }
--- sm14-3/main.gdb
+++ sm14-3/main.gdb
@@ -1,2 +1,8 @@
 b main
-r .
+#b traverse
+b main.c:45
+b main.c:50
+b main.c:58
+b main.c:63
+#b main.c:242
+r ~
