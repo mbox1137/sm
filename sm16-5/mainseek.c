61ffbdbf45afc91f9f41713b4b878fb191814df1
Alex B <alexandr.belonozhkin@gmail.com>
2020-04-29 22:41:50 +0300

    sm16-5: !

--- sm16-5/mainseek.c
+++ sm16-5/mainseek.c
@@ -1,5 +1,4 @@
-#define DEBUG 1
-#define WRITE 1
+#define DEBUG 0
 
 //http://www.opennet.ru/base/dev/unix_signals.txt.html
 
@@ -13,22 +12,15 @@
     #include <malloc.h>
     #include <fcntl.h>
 
-    int startArithmeticProgression(int h, int a0, int d, int k) {	//pid
-        pid_t cpid, mypid;
-        int a;
+    int startArithmeticProgression(int h, int i, int n, int a0, int d, int k) { //pid
+        pid_t cpid;
+        int a, j;
         cpid=fork();
         if(cpid==0) {	//child
             a=a0;
-            mypid=getpid();
-            for(;;) {
-//                usleep(0.5E6);
-                kill(mypid, SIGSTOP);
-#if WRITE
-//        lseek(h, n*k*sizeof(int), SEEK_SET);
+            for(j=0;j<k;j++) {
+                lseek(h, (i+j*n)*sizeof(int), SEEK_SET);
                 write(h, &a, sizeof(int));
-#else
-                printf("%d: a=%d\n", mypid, a);
-#endif
                 a+=d;
             }
         } else {	//parent
@@ -39,11 +31,10 @@
     int
     main(int argc, char *argv[])
     {
-        int n, a0, d, k, i, j;
+        int n, a0, d, k, i;
         int h;
         char f[132];
         pid_t *cpids;
-        siginfo_t infop;
 
         n=3;
         strcpy(f,"out.bin");
@@ -53,45 +44,14 @@
 
 #if DEBUG
         printf("N=%d F=%s A0=%d D=%d K=%d\n",n,f,a0,d,k);
-//        sleep(1);
 #endif
-//----------------------------------------------- без пайпов!!!
         h=creat(f,0664);
         cpids=malloc(n*sizeof(pid_t));
         if(cpids==NULL) {
             return(-1);
         }
         for(i=0; i<n; i++) {
-            cpids[i]=startArithmeticProgression(h, a0+d*i, d*n, k);
-        }
-        for(j=0; j<k; j++) {
-            for(i=0; i<n; i++) {
-                for(;;) {
-                    infop.si_code=0;
-                    waitid(P_PID, cpids[i], &infop, WSTOPPED);
-                    if(infop.si_code==CLD_STOPPED)
-                        break;
-                }
-                kill(cpids[i], SIGCONT);
-                for(;;) {
-                    infop.si_code=0;
-                    waitid(P_PID, cpids[i], &infop, WCONTINUED);
-                    if(infop.si_code==CLD_CONTINUED)
-                        break;
-                }
-                usleep(499);
-            }
-        }
-        for(i=0; i<n; i++) {
-            for(;;) {
-                waitid(P_PID, cpids[i], &infop, WEXITED|WSTOPPED);
-                if(infop.si_code==CLD_STOPPED || infop.si_code==CLD_EXITED)
-                    break;
-            }
-        }
-        for(i=0; i<n; i++) {
-            kill(cpids[i], SIGTERM);
-            kill(cpids[i], SIGCONT);
+            cpids[i]=startArithmeticProgression(h, i, n, a0+d*i, d*n, k);
         }
         wait(NULL);
         free(cpids);
