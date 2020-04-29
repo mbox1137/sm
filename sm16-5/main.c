83a6ad91e13d2b50f6aff64308c8d4005f484036
Alex B <alexandr.belonozhkin@gmail.com>
2020-04-29 21:24:46 +0300

    sm16-5: _

--- sm16-5/main.c
+++ sm16-5/main.c
@@ -1,17 +1,8 @@
 #define DEBUG 1
-//http://www.opennet.ru/base/dev/unix_signals.txt.html
-/*
-man 2 pipe
+#define WRITE 1
 
-EXAMPLE
-    The  following   program     creates     a pipe, and then fork(2)s to create a
-    child process; the child inherits a duplicate set of    file    descriptors
-    that    refer    to  the same pipe. After the fork(2), each process closes
-    the file descriptors that it doesn't need for the pipe  (see    pipe(7)).
-    The  parent  then writes the string contained in the program's command-
-    line argument to the pipe, and the child reads this string a byte at    a
-    time from the pipe and echoes it on standard output.
-*/
+//http://www.opennet.ru/base/dev/unix_signals.txt.html
+
     #include <signal.h>
     #include <sys/types.h>
     #include <sys/wait.h>
