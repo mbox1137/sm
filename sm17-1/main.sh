629bab87ae2d6bf0a42d0e8192c0dd3e6324a690
Alex B <alexandr.belonozhkin@gmail.com>
2020-05-01 22:34:09 +0300

    sm17-1

---
+++ sm17-1/main.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#./main 3 out.bin 4 2 4
+function myrun {
+	./mainseek $@
+	if ! ./check $@
+	then
+		echo $@
+		ls -la $2
+		hexdump -d $2
+		echo
+	fi
+}
+
+myrun 3 out3x4.bin 4 2 4
+#myrun 3 out3x40.bin 4 2 40
+#myrun 30 out30x4.bin 4 2 4
+#myrun 30 out30x40.bin 4 2 40
