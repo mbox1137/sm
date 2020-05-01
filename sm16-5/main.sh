f5b95ad560060d249feb75399217a5a45ac40d6b
Alex B <alexandr.belonozhkin@gmail.com>
2020-05-01 19:52:56 +0300

    sm16-5: _

--- sm16-5/main.sh
+++ sm16-5/main.sh
@@ -6,12 +6,12 @@ function myrun {
 	then
 		echo $@
 		ls -la $2
-#		hexdump -d $2
+		hexdump -d $2
 		echo
 	fi
 }
 
-myrun 3 out.bin 4 2 4
-myrun 3 out.bin 4 2 40
-myrun 30 out.bin 4 2 4
-myrun 30 out.bin 4 2 40
+myrun 3 out3x4.bin 4 2 4
+#myrun 3 out3x40.bin 4 2 40
+#myrun 30 out30x4.bin 4 2 4
+#myrun 30 out30x40.bin 4 2 40
