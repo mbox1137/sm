0d0ab6ba6b9efce2b41153058faec958b2f90fa9
sezentt <drunchic12@mail.ru>
2020-05-03 20:27:07 +0300

    16-5: изменили wait, добавили ftruncate (2 ошибки в теста вместо одной)

--- sm16-5/mainseek.c
+++ sm16-5/mainseek.c
@@ -1,6 +1,4 @@
 #define DEBUG 0
-#define NN 10
-#define _GNU_SOURCE 1
 
 #include <signal.h>
 #include <sys/types.h>
@@ -11,57 +9,18 @@
 #include <string.h>
 #include <malloc.h>
 #include <fcntl.h>
-#include <sys/file.h>
 
 int startArithmeticProgression(int h, int i, int n, int a0, int d, int k) { //pid
     pid_t cpid;
     int a, j;
-    off_t off;
-    size_t siz;
-    int nn, nnn;
-
     cpid = fork();
     if (cpid == 0)
     {
+        a = a0;
         for(j = 0; j < k; j++)
         {
-            for(nn=0;nn<NN;nn++) {
-                printf(" j%d",j);
-                if(flock(h, LOCK_EX))
-                    usleep(10);
-                else
-                    break;
-            }
-            if(nn==NN){
-            }
-            for(nnn=0;nnn<NN;nnn++) {
-                off=(i + j * n) * sizeof(int);
-                for(nn=0;nn<NN;nn++)
-                    if(lseek(h, off, SEEK_SET)==off)
-                        break;
-                if(nn==NN){
-                }
-                a = a0+j*d;
-                siz=sizeof(int);
-                for(nn=0;nn<NN;nn++) {
-                    printf(" w%d",j);
-                    if(write(h, &a, siz)==siz)
-                        break;
-                }
-                if(nn==NN){
-                }
-            }
-            sync();
-//            syncfs(h);
-            for(nn=0;nn<NN;nn++) {
-                printf(" J%d",j);
-                if(flock(h, LOCK_UN))
-                    usleep(10);
-                else
-                    break;
-            }
-            if(nn==NN){
-            }
+            lseek(h, (i + j * n) * sizeof(int), SEEK_SET);
+            write(h, &a, sizeof(int));
             a += d;
         }
         _exit(EXIT_SUCCESS);
@@ -75,6 +34,7 @@ int main(int argc, char *argv[])
     int h;
     char f[132];
     pid_t *cpids;
+    int pstat;
 
     if (argc == 1)
     {
@@ -102,6 +62,7 @@ int main(int argc, char *argv[])
     printf("N=%d F=%s A0=%d D=%d K=%d\n", n, f, a0, d, k);
 #endif
     h = creat(f, S_IRUSR|S_IWUSR| S_IRGRP| S_IROTH);
+    ftruncate(h, n * k * sizeof(int));
 
     cpids = malloc(n * sizeof(pid_t));
     if(cpids == NULL)
@@ -110,7 +71,24 @@ int main(int argc, char *argv[])
     for(i = 0; i < n; i++)
         cpids[i] = startArithmeticProgression(h, i, n, a0 + d * i, d * n, k);
 
-    wait(NULL);
+    //wait(NULL);
+    for(i = 0; i < n; i++)
+    {
+        waitpid(cpids[i], &pstat, 0);
+        /*if (WIFEXITED(pstat))
+        {
+            exitstatus = WEXITSTATUS(pstat);
+            if (exitstatus)
+            {
+                if (mypid==getpid())
+                {
+                    printf("-1\n");
+                    exit(EXIT_SUCCESS);
+                }
+                exit(EXIT_FAILURE);
+            }
+        }*/
+    }
 
     free(cpids);
 
@@ -120,4 +98,3 @@ int main(int argc, char *argv[])
 }
 
 // http://www.opennet.ru/base/dev/unix_signals.txt.html
-
