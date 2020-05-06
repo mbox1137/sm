817a759bf29a7c1abfaaabd54846917d6b18dfdb
sezentt <drunchic12@mail.ru>
2020-05-06 18:07:00 +0300

    17-1: попробовал попровить, осталось 2 ошибки

--- sm17-1/prog.c
+++ sm17-1/prog.c
@@ -12,27 +12,27 @@ int main(int argc, char *argv[])
 {
     char *cmd;
     int outfd, infd;
-    int k;
-    int status;
+    //int status;
     pid_t pid;
 
     if (argc != 4)
     {
-        for(k = 1; k < argc; k++)
+        /*for(int k = 1; k < argc; k++)
             fprintf(stderr,"%d %s\n", k, argv[k]);
-        fprintf(stderr,"./prog wc in.txt out.txt\n");
-        return 1;
-    } else {
-        cmd = argv[1];
-        infd = open(argv[2], O_RDONLY);
-        outfd = open(argv[3], O_CREAT|O_WRONLY|O_TRUNC, 0644);
+        fprintf(stderr,"./prog wc in.txt out.txt\n");*/
+        exit(1);
     }
+
+    cmd = argv[1];
+    infd = open(argv[2], O_RDONLY);
+    outfd = open(argv[3], O_CREAT|O_WRONLY|O_TRUNC, 0644);
 
     if (!infd)
     {
         perror("open infd");
         return EXIT_FAILURE;
     }
+
     if (!outfd)
     {
         perror("open outfd");
@@ -40,28 +40,37 @@ int main(int argc, char *argv[])
     }
 
     pid = fork();
-    if (pid < 0)
+
+    /*if (pid < 0)
     {
         close(infd);
         close(outfd);
         perror("fork");
         return EXIT_FAILURE;
-    }
+    }*/
 
-    if(!pid)
+    if (!pid)
     {
         dup2(infd, 0);
         close(infd);
+
         dup2(outfd, 1);
         close(outfd);
-        execlp(cmd, "-", NULL);
-    } else {
+
+        execlp(cmd, cmd, NULL);
+        exit(1);
+    }
+    /*else {
         close(infd);
         close(outfd);
         waitpid(pid, &status, 0);
         if (WIFEXITED(status)) return WEXITSTATUS(status);
         else return EXIT_FAILURE;
-    }
+    }*/
+
+    wait(NULL);
+
+    return 0;
 }
 
 // https://stackoverflow.com/questions/44221222/how-to-use-execlp-with-redirected-output
