a50809d1a86c75d1b02450bbdd6b6017237d1f21
Alex B <alexandr.belonozhkin@gmail.com>
2020-05-08 15:45:34 +0300

    sm18-4 !

--- sm18-4/solution.c
+++ sm18-4/solution.c
@@ -1,4 +1,4 @@
-#define DEBUG 1
+#define DEBUG 0
 //https://stackoverflow.com/questions/43349397/why-fprintf-and-fscanf-does-not-work-with-pipe
 #define _POSIX_C_SOURCE 200101L
 #include <stdlib.h>
@@ -9,9 +9,11 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-int startPingPong(FILE* fin, FILE* fout, int n, int nn, const int *pn0) {
+int startPingPong(int* fdin, int* fdout, int n, int nn, const int *pn0) {
     pid_t pid;
     int x;
+    FILE* fin;
+    FILE* fout;
 
     pid=fork();
     if(pid == -1) {
@@ -19,6 +21,10 @@ int startPingPong(FILE* fin, FILE* fout, int n, int nn, const int *pn0) {
         exit(EXIT_FAILURE);
     }
     if (pid == 0) {
+        fin = fdopen(fdin[0], "r");
+        close(fdin[1]);
+        fout = fdopen(fdout[1], "w");
+        close(fdout[0]);
         if(pn0) {
             fprintf(fout, "%d\n", 1);
             fflush(fout);
@@ -28,8 +34,8 @@ int startPingPong(FILE* fin, FILE* fout, int n, int nn, const int *pn0) {
             fprintf(fout, "%d\n", x+1);
             fflush(fout);
         }
-//        fclose(fin);
-//        fclose(fout);
+        fclose(fin);
+        fclose(fout);
         exit(EXIT_SUCCESS);
     } else {
         return(pid);
@@ -54,25 +60,14 @@ int main(int argc, char** argv) {
 
     pipe(fd12);
     pipe(fd21);
-/*
-    FILE* wf12 = fdopen(fd12[1], "w");
-    FILE* rf12 = fdopen(fd12[0], "r");
-    FILE* wf21 = fdopen(fd21[1], "w");
-    FILE* rf21 = fdopen(fd21[0], "r");
-*/
+
     cpids[0]=startPingPong(fd21, fd12, 1, nn, NULL);
     cpids[1]=startPingPong(fd12, fd21, 2, nn, &dummy);
-/*
-    fclose(wf12);
-    fclose(rf12);
-    fclose(wf21);
-    fclose(rf21);
-    
+
     close(fd12[0]);
     close(fd12[1]);
     close(fd21[0]);
     close(fd21[1]);
-*/
 #if DEBUG
     printf("cpid[0]=%d cpid[1]=%d\n",cpids[0], cpids[1]);
 #endif
@@ -82,88 +77,5 @@ int main(int argc, char** argv) {
         if (WIFEXITED(status)) continue;	//return WEXITSTATUS(status);
         else return EXIT_FAILURE;
     }
-//    printf("Done\n");
-    fprintf(stderr, "Done\n");
+    printf("Done\n");
 }
-
-/*
-PIPE(2)                    Linux Programmer's Manual                   PIPE(2)
-
-NAME
-       pipe, pipe2 - create pipe
-
-SYNOPSIS
-       #include <unistd.h>
-
-       struct fd_pair {
-           long fd[2];
-       };
-       struct fd_pair pipe();
-
-       int pipe(int pipefd[2]);
-
-       #define _GNU_SOURCE
-       #include <fcntl.h>
-       #include <unistd.h>
-
-       int pipe2(int pipefd[2], int flags);
-
-EXAMPLE
-       The  following  program  creates  a pipe, and then fork(2)s to create a
-       child process; the child inherits a duplicate set of  file  descriptors
-       that  refer  to  the same pipe.  After the fork(2), each process closes
-       the file descriptors that it doesn't need for the pipe  (see  pipe(7)).
-       The  parent  then writes the string contained in the program's command-
-       line argument to the pipe, and the child reads this string a byte at  a
-       time from the pipe and echoes it on standard output.
-
-   Program source
-       #include <sys/types.h>
-       #include <sys/wait.h>
-       #include <stdio.h>
-       #include <stdlib.h>
-       #include <unistd.h>
-       #include <string.h>
-
-       int
-       main(int argc, char *argv[])
-       {
-           int pipefd[2];
-           pid_t cpid;
-           char buf;
-
-           if (argc != 2) {
-               fprintf(stderr, "Usage: %s <string>\n", argv[0]);
-               exit(EXIT_FAILURE);
-           }
-
-           if (pipe(pipefd) == -1) {
-               perror("pipe");
-               exit(EXIT_FAILURE);
-           }
-
-           cpid = fork();
-           if (cpid == -1) {
-               perror("fork");
-               exit(EXIT_FAILURE);
-           }
-
-           if (cpid == 0) {
-               close(pipefd[1]);
-
-               while (read(pipefd[0], &buf, 1) > 0)
-                   write(STDOUT_FILENO, &buf, 1);
-
-               write(STDOUT_FILENO, "\n", 1);
-               close(pipefd[0]);
-               _exit(EXIT_SUCCESS);
-
-           } else {
-               close(pipefd[0]);
-               write(pipefd[1], argv[1], strlen(argv[1]));
-               close(pipefd[1]);
-               wait(NULL);
-               exit(EXIT_SUCCESS);
-           }
-       }
-*/
