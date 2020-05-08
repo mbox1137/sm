05f2a2322aa4254e7187c92eb2a446ae56781439
Alex B <alexandr.belonozhkin@gmail.com>
2020-05-08 22:20:30 +0300

    sm17-5: _

--- sm17-5/mysystem.c
+++ sm17-5/mysystem.c
@@ -1,80 +1,148 @@
 #define DEBUG 1
 
 #include <stdio.h>
-#include "mysystem.h"
-
-int mysystem(const char *str) {
-#if DEBUG
-    fprintf(stderr,"cmd=\"%s\"\n",str);
-#endif
-    return(0);
-}
-
-/*
-#define _POSIX_C_SOURCE 200101L
+#include <string.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <fcntl.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[])
-{
+#include "mysystem.h"
+
+void addname(CTRLN *z, char *name) {
+    int n;
+    if(!name) {
+        if(z->names)
+            free(z->names);
+        if(z->pnames)
+            free(z->pnames);
+        z->kn=0;
+        z->nn=0;
+        z->kpn=0;
+        z->npn=0;
+        return;
+    }
+    n=strlen(name);
+    if(n==0) {
+        z->kn=0;
+        z->kpn=0;
+        return;
+    }
+    if((z->kn+n+1) >= (z->nn)) {
+        if(!z->names) {
+            z->nn = 1<<13;
+            z->names = malloc(z->nn);
+            if(z->names == NULL)
+                z->names = NULL;
+        } else {
+            z->nn *= 2;
+            z->names = realloc(z->names, z->nn);
+            if(z->names == NULL)
+                z->names = NULL;
+        }
+    }
+    if((z->kpn) >= (z->npn)) {
+        if(!z->pnames) {
+            z->npn = 1<<10;
+            z->pnames = malloc((z->npn) * sizeof(int));
+            if(z->pnames == NULL)
+                z->pnames = NULL;
+        } else {
+            z->npn *= 2;
+            z->pnames = realloc(z->pnames, z->npn*sizeof(int));
+            if(z->pnames == NULL)
+                z->pnames = NULL;
+        }
+    }
+    (z->pnames)[z->kpn]=z->kn;
+    strcpy(&((z->names)[z->kn]),name);
+    z->kn += n+1;
+    z->kpn++;
+}
+
+int mysystem(const char *str) {
     char *cmd;
-    int outfd, infd;
-    int k;
     int status;
     pid_t pid;
+    CTRLN ctrln;
+    char delim[]=" \t";
+    char *cp;
+    char **args;
+    int k;
 
-    if(argc != 4) {
-        for(k=1;k<argc;k++)
-            fprintf(stderr,"%d %s\n", k, argv[k]);
-        fprintf(stderr,"./prog wc in.txt out.txt\n");
+    ctrln.names=NULL;
+    ctrln.pnames=NULL;
+    addname(&ctrln, NULL);	//init
+    cp = strtok((char*)str, delim);
+    do {
+        addname(&ctrln, cp);
+    } while(cp=strtok(NULL, delim));
+    args=malloc((ctrln.kpn+2)*sizeof(char*));
+    if(args==NULL) {
+    }
+    args[0]=&ctrln.names[ctrln.pnames[0]];
+    for (k = 0; k < ctrln.kpn; k++)
+        args[k+1]=&ctrln.names[ctrln.pnames[k]];
+    args[k+1]=NULL;
+    free(args);
+
+
+#if DEBUG
+    if(str)
+        fprintf(stderr,"cmd=\"%s\"\n",str);
+#endif
+
+    pid = fork();
+
+    if (pid < 0)
+    {
         return(1);
-    } else {
-        cmd=argv[1];
-        infd = open(argv[2], O_RDONLY);
-        outfd = open(argv[3], O_CREAT|O_WRONLY|O_TRUNC, 0644);
     }
 
+    if (!pid)
+    {
+        execlp(cmd, cmd, NULL);
+        exit(EXIT_SUCCESS);
+    } else {
+        waitpid(pid, &status, 0);
+        if (WIFEXITED(status)) exit(WEXITSTATUS(status));
+        else exit(EXIT_FAILURE);
+    }
+    addname(&ctrln, NULL);	//finit
+    return(0);
+}
+
+/*
+#define DEBUG 0
+
+int main(int argc, char *argv[])
+{
+    int outfd, infd;
+
+    if (argc != 4)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    cmd = argv[1];
+
+    infd = open(argv[2], O_RDONLY);
     if (!infd)
     {
         perror("open infd");
-        return EXIT_FAILURE;
+        exit(EXIT_FAILURE);
     }
+
+    outfd = open(argv[3], O_CREAT|O_WRONLY|O_TRUNC, 0666);
     if (!outfd)
     {
         perror("open outfd");
-        return EXIT_FAILURE;
-    }
-    pid = fork();
-    if (pid < 0)
-    {
-        close(infd);
-        close(outfd);
-        perror("fork");
-        return EXIT_FAILURE;
+        exit(EXIT_FAILURE);
     }
 
-    if(!pid)
-    {
-        // child code
-        dup2(infd, 0); // replace stdin
-        close(infd);
-        dup2(outfd, 1); // replace stdout
-        close(outfd);
-        execlp(cmd, "-", NULL);
-    }
-    else
-    {
-        // parent code
-        close(infd);
-        close(outfd);
-        waitpid(pid, &status, 0);
-        if (WIFEXITED(status)) return WEXITSTATUS(status);
-        else return EXIT_FAILURE;
-    }
+
+    return 0;
 }
 */
--- sm17-5/mysystem.h
+++ sm17-5/mysystem.h
@@ -1,1 +1,10 @@
 int mysystem(const char *str);
+typedef struct {
+    char *names;
+    int *pnames;
+    int kn;
+    int nn;
+    int kpn;
+    int npn;
+    } CTRLN;
+void addname(CTRLN *z, char *name);
