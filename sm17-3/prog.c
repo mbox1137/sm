532b1b3d1bb329e8805361d3bafb99c5384b22cd
sezentt <drunchic12@mail.ru>
2020-05-09 19:46:06 +0300

    sm17-3: need tests

--- sm17-3/prog.c
+++ sm17-3/prog.c
@@ -1,5 +1,3 @@
-#define DEBUG 0
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/types.h>
@@ -8,75 +6,42 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+int process(char *cmd)
+{
+    pid_t proc;
+    int wstatus;
+
+    proc = fork();
+
+    if (!proc)
+    {
+        execlp(cmd, cmd, NULL);
+        return 0;
+    }
+
+    waitpid(proc, &wstatus, 0);
+
+    if (WIFEXITED(wstatus))
+        return 1 && !WEXITSTATUS(wstatus);
+    else
+        return 0;
+}
+
 int main(int argc, char *argv[])
 {
-    return 0;
-}
-
-
-int main_(int argc, char *argv[])
-{
-    char *cmd;
-    int outfd, infd;
-    //int status;
-    pid_t pid;
-
     if (argc != 4)
-    {
-        /*for(int k = 1; k < argc; k++)
-            fprintf(stderr,"%d %s\n", k, argv[k]);
-        fprintf(stderr,"./prog wc in.txt out.txt\n");*/
-        exit(1);
-    }
+        exit(EXIT_FAILURE);
 
-    cmd = argv[1];
-    infd = open(argv[2], O_RDONLY);
-    outfd = open(argv[3], O_CREAT|O_WRONLY|O_TRUNC, 0644);
+    if (process(argv[1]))
+        if (process(argv[3]))
+            return 0;
+        else
+            return 1;
+    else if (process(argv[2]))
+        if (process(argv[3]))
+            return 0;
+        else
+            return 1;
 
-    if (!infd)
-    {
-        perror("open infd");
-        return EXIT_FAILURE;
-    }
-
-    if (!outfd)
-    {
-        perror("open outfd");
-        return EXIT_FAILURE;
-    }
-
-    pid = fork();
-
-    /*if (pid < 0)
-    {
-        close(infd);
-        close(outfd);
-        perror("fork");
-        return EXIT_FAILURE;
-    }*/
-
-    if (!pid)
-    {
-        dup2(infd, 0);
-        close(infd);
-
-        dup2(outfd, 1);
-        close(outfd);
-
-        execlp(cmd, cmd, NULL);
-        exit(1);
-    }
-    /*else {
-        close(infd);
-        close(outfd);
-        waitpid(pid, &status, 0);
-        if (WIFEXITED(status)) return WEXITSTATUS(status);
-        else return EXIT_FAILURE;
-    }*/
-
-    wait(NULL);
-
-    return 0;
+    return 1;
 }
-
-// https://stackoverflow.com/questions/44221222/how-to-use-execlp-with-redirected-output
