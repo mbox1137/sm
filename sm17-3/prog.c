30a582bbf7789ec21395dcdb6da3275d8226a8ab
sezentt <drunchic12@mail.ru>
2020-05-10 11:28:28 +0300

    sm17-3: Makefile done!

--- sm17-3/prog.c
+++ sm17-3/prog.c
@@ -1,3 +1,5 @@
+#define DEBUG 0
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/types.h>
@@ -11,11 +13,15 @@ int process(char *cmd)
     pid_t proc;
     int wstatus;
 
+#if DEBUG
+    printf("cmd = %s\n", cmd);
+#endif
+
     proc = fork();
 
     if (!proc)
     {
-        execlp(cmd, cmd, NULL);
+        execlp("/bin/sh", "sh", "-c", cmd, NULL);
         return 0;
     }
 
@@ -29,19 +35,28 @@ int process(char *cmd)
 
 int main(int argc, char *argv[])
 {
+#if DEBUG
+    printf("%d\n", argc);
+#endif
+
     if (argc != 4)
         exit(EXIT_FAILURE);
 
     if (process(argv[1]))
+    {
         if (process(argv[3]))
             return 0;
         else
             return 1;
+    }
+
     else if (process(argv[2]))
+    {
         if (process(argv[3]))
             return 0;
         else
             return 1;
+    }
 
     return 1;
 }
--- sm17-3/prog.sh
+++ sm17-3/prog.sh
@@ -3,9 +3,20 @@
 TMPF=tmp.txt
 function myrun {
 	echo $@
-	./prog $1 $2 $3
+	./prog "$1" "$2" "$3"
 #	$1 <$2 >$TMPF
 #	cmp $3 $TMPF
 }
 
-myrun ls "ls -la" true
+myrun ls "ls -la" true; echo $?
+echo
+myrun true false true; echo $?
+
+myrun false true true; echo $?
+
+myrun true false false; echo $?
+
+myrun false false true; echo $?
+
+myrun true true false; echo $?
+
