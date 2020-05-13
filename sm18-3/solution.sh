c9e1747bf8e4058daf2f647ffa73b3ba060daa0b
Alex B <alexandr.belonozhkin@gmail.com>
2020-05-13 10:00:41 +0300

    sm18-3: Перепутаны CMD1, CMD2 ?

--- sm18-3/solution.sh
+++ sm18-3/solution.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-#./solution 3 out.bin 4 2 4
-CMD1="ls"
-CMD2="pwd"
-CMD3="wc"
-INF=in.tst
-TMPF=tmp.txt
-FILE=out.txt
+
+FILE=out.log
+TMPF=tmp.log
 function myrun {
 	echo $@
-	./solution $1 $2 >$FILE
-	$1 |$2 >$TMPF
-	cmp $FILE $TMPF
+	./solution $@
+	( $1 ; $2 ) | $3 > $TMPF
+	if ! cmp $FILE $TMPF
+	then
+		diff -y $FILE $TMPF
+	fi
 }
 
-./solution $CMD1 $CMD2 $CMD3 $FILE
-( $CMD1 ; $CMD2 ) | $CMD3 > $TMPF
-cmp $FILE $TMPF
+myrun ls pwd wc $FILE
+myrun ls pwd cat $FILE
