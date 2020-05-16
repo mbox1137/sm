540a52ec0e8ee5806a8b6bdce26f305761a106da
sezentt <drunchic12@mail.ru>
2020-05-16 04:55:41 +0300

    sm18-5: styling only

--- sm18-5/solution.c
+++ sm18-5/solution.c
@@ -15,110 +15,144 @@
 
 //https://stackoverflow.com/questions/16639275/grouping-child-processes-with-setgpid
 
-int runItem(int *lp, int *rp, char* cmd) {
+int runItem(int *lp, int *rp, char* cmd)
+{
     pid_t pid, pgid;
     pid = fork();
+
     if (pid < 0)
-        return(-1);
-    pgid=setpgid(getpid(), getpgid(getppid()));
+        return -1;
+
+    pgid = setpgid(getpid(), getpgid(getppid()));
+
     if (pgid < 0)
-        return(-1);
+        return -1;
+
     if (!pid)
     {
-        if(lp[0]) {
+        if (lp[0])
+        {
             dup2(lp[0], 0);
             close(lp[0]);
-            lp[0]=0;
+            lp[0] = 0;
         }
-        if(lp[1]) {
+
+        if (lp[1])
+        {
             close(lp[1]);
-            lp[1]=0;
+            lp[1] = 0;
         }
-        if(rp[1]) {
+
+        if (rp[1])
+        {
             dup2(rp[1], 1);
             close(rp[1]);
-            rp[1]=0;
+            rp[1] = 0;
         }
+
         execlp(cmd, cmd, NULL);
         exit(EXIT_FAILURE);
     } else {
-        if(lp[0]) {
+        if (lp[0])
+        {
             close(lp[0]);
-            lp[0]=0;
+            lp[0] = 0;
         }
-        if(lp[1]) {
+
+        if (lp[1])
+        {
             close(lp[1]);
-            lp[1]=0;
+            lp[1] = 0;
         }
-        if(rp[1]) {
+
+        if (rp[1])
+        {
             close(rp[1]);
-            rp[1]=0;
+            rp[1] = 0;
         }
-        return(pid);
+
+        return pid;
     }
 }
 
-int solution (int argc, char *argv[]) {
+int solution (int argc, char *argv[])
+{
     pid_t *pids, pgid;
     int k, k0;
     int status, retval;
     int lp[2], rp[2];
+
 #if DEBUG
-    for (k = 0; k<argc; k++) {
+    for (k = 0; k < argc; k++)
         printf("argv[%d]=\t\"%s\"\n", k, argv[k]);
-    }
 #endif
-    if(argc<1)
-        return(0);
-    k=-1;
-    pids=malloc(argc*sizeof(pid_t));
-    if(!pids)
+
+    if (argc < 1)
+        return 0;
+
+    k = -1;
+    pids = malloc(argc * sizeof(pid_t));
+
+    if (!pids)
         goto closeAll;
-    if(argc>1) {
-        pgid=setpgid(getpid(), getpid());
+
+    if (argc > 1)
+    {
+        pgid = setpgid(getpid(), getpid());
+
         if (pgid < 0)
             goto closeAll;
-        lp[0]=0;
-        lp[1]=0;
-        for (k = 0; k<argc-1; k++) {
+
+        lp[0] = 0;
+        lp[1] = 0;
+        for (k = 0; k < argc - 1; k++)
+        {
             pipe(rp);
-            pids[k]=runItem(lp, rp, argv[k]);
-            lp[0]=rp[0];
-            lp[1]=rp[1];
-            if(pids[k]==-1)
+            pids[k] = runItem(lp, rp, argv[k]);
+            lp[0] = rp[0];
+            lp[1] = rp[1];
+
+            if (pids[k] == -1)
                 goto closeAll;
         }
-        lp[0]=rp[0];
-        lp[1]=rp[1];
-        rp[0]=0;
-        rp[1]=0; 
-        pids[k]=runItem(lp, rp, argv[k]);
-        if(pids[k]==-1)
+
+        lp[0] = rp[0];
+        lp[1] = rp[1];
+        rp[0] = 0;
+        rp[1] = 0;
+        pids[k] = runItem(lp, rp, argv[k]);
+        if (pids[k] == -1)
             goto closeAll;
     } else {
-        k=0;
-        lp[0]=0;
-        lp[1]=0;
-        rp[0]=0;
-        rp[1]=0; 
-        pids[k]=runItem(lp, rp, argv[k]);
+        k = 0;
+        lp[0] = 0;
+        lp[1] = 0;
+        rp[0] = 0;
+        rp[1] = 0;
+        pids[k] = runItem(lp, rp, argv[k]);
+
 #if ERRTEST
         goto closeAll;
 #else
-        if(pids[k]==-1)
+        if (pids[k] == -1)
             goto closeAll;
 #endif
     }
-    k0=k+1;
+
+    k0 = k + 1;
     retval = 0;
     goto closeExit;
+
 closeAll:
-    k0=k+1;
+    k0 = k+1;
     retval = 1;
-    for (k = 0; k<k0; k++)
+    for (k = 0; k < k0; k++)
         kill(pids[k], SIGKILL);
-closeExit:    
-    for (k = 0; k<k0; k++) {
+
+closeExit:
+    for (k = 0; k < k0; k++)
+    {
+
 #if DEBUG
         printf("Waiting for %d (%d)\n", k, pids[k]);
 #endif
@@ -127,5 +161,5 @@ closeExit:
             continue;
     }
     free(pids);
-    return(retval);
+    return retval;
 }
