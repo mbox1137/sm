18536480efbcfb4016700fac34c35305819a9f73
Alex B <alexandr.belonozhkin@gmail.com>
2020-05-19 18:35:48 +0300

    sm19-4: _

---
+++ sm19-4/test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+def getnum():
+    while 1:
+        s=sys.stdin.readline()
+        if len(s)==0: break
+        s=s.strip()
+        sl=s.split()
+        for s in sl:
+            yield int(s)**2
+
+for w in getnum():
+    print(w)
