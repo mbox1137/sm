47439359442902ed81504a0bad8eb7da8005ffec
Alex B <alexandr.belonozhkin@gmail.com>
2020-05-19 19:34:35 +0300

    sm19-4: _

--- sm19-4/check.py
+++ sm19-4/check.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
+import sys
 import subprocess
 
-args = ["ping","-c 3","www.ru"]
+args = ["./test.py"]
 proc = subprocess.Popen(args, 
                         stdin=subprocess.PIPE,
                         stdout=subprocess.PIPE,
-                        stderr=subprocess.PIPE)
+                        stderr=subprocess.PIPE,
+                        bufsize=1,
+                        universal_newlines=True)
  
 #data = process.communicate()
+sys.stdout.write('1 2 3\n')
+proc.stdin.write('1 2 3\n')
+print(proc.stdin, 4,5,6)
+proc.stdin.close()
 for line in proc.stdout:
-    print(line.decode("utf-8").strip())
+#    print(line.decode("utf-8").strip())
+    print(line.strip())
