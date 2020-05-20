2d04418671a8e4bff47064a13d125fb346d685f6
Alex B <alexandr.belonozhkin@gmail.com>
2020-05-20 12:56:42 +0300

    sm19-4: _

--- sm19-4/check.py
+++ sm19-4/check.py
@@ -2,13 +2,19 @@
 import sys
 import signal, subprocess
 
-args = ["./test.py"]
+if len(sys.argv) < 2:
+    print("./check.py ./test.py")
+    sys.exit()
+args = sys.argv
+args.pop(0)
 proc = subprocess.Popen(args, 
                         stdin=subprocess.PIPE,
                         stdout=subprocess.PIPE,
                         stderr=subprocess.PIPE,
                         bufsize=1,
                         universal_newlines=True)
+cpid=int(proc.stdout.readline())
+print(f"CPID={cpid}")
 print(4,5,6, file=proc.stdin)
 proc.stdin.close()
 for line in proc.stdout:
--- sm19-4/test.py
+++ sm19-4/test.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 #https://stackabuse.com/handling-unix-signals-in-python/
-import os, sys
+import os, sys, builtins, time
 import signal
+
+def print(*args, **kwargs):
+    builtins.print(*args, **kwargs)
+    sys.stdout.flush()
+#    time.sleep(0.1)
 
 def getnum():
     while 1:
@@ -12,7 +17,7 @@ def getnum():
         for s in sl:
             yield int(s)**2
 
+print(os.getpid())
+
 for w in getnum():
     print(w)
-
-print(f"PID={os.getpid()}")
