990b5f1aadbdaebfc91ddec821139ad012ea49f1
Alex B <alexandr.belonozhkin@gmail.com>
2020-05-31 21:10:58 +0300

    sm20-3: !

--- sm20-3/task.c
+++ sm20-3/task.c
@@ -1,4 +1,4 @@
-#define DEBUG 1
+#define DEBUG 0
 
 //Unix Socket - Server Examples
 //https://www.tutorialspoint.com/unix_sockets/socket_server_example.htm
@@ -6,41 +6,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
 #include <netdb.h>
 #include <netinet/in.h>
-
 #include <string.h>
-
 #include "server.h"
 
 int task (int sock) {
-   int n, num;
+   int n, nn, num;
    char buf[256];
    char str[80];
    pid_t mypid;
 
    mypid=getpid();
    bzero(buf,256);
-   n = read(sock,buf,255);
+#if DEBUG
+   nn=255;
+   n = read(sock,buf,nn);
+   if (n == 0) return(0);
    if (n < 0) {
       sprintf(str, "%d: ERROR reading NUM", mypid);
       perror(str);
       close(sock);
       exit(1);
    }
-   if (n == 0) return(0);
-#if DEBUG
-   fprintf(stderr, "%d: buf(NUM)=%s\n", mypid, buf);
-#endif
    if(sscanf(buf, "%d", &num)!=1) {
       sprintf(str, "%d: Invalid num", mypid);
       perror(str);
       close(sock);
       exit(1);
    }
-#if DEBUG
    fprintf(stderr, "%d: num=%d\n", mypid, num);
+#else
+   nn=4;
+   n = read(sock,buf,nn);
+   if (n != nn) {
+      sprintf(str, "%d: ERROR reading NUM", mypid);
+      perror(str);
+      close(sock);
+      exit(1);
+   }
+   if (n == 0) return(0);
+   num=ntohl(*((int*)buf));
 #endif
    close(sock);
    return(num);
