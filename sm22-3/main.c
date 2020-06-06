0453d502763af23b1a85e65f1c93dfed76dd69fe
Alex B <alexandr.belonozhkin@gmail.com>
2020-06-06 20:49:39 +0300

    sm22-3: _

--- sm22-3/main.c
+++ sm22-3/main.c
@@ -1,3 +1,5 @@
+#define DEBUG 1
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -8,33 +10,42 @@ typedef struct {
 } args_t;
 
 void* tfun(void *args) {
-    pthread_t thread;
-    int status;
-    int status_addr;
-    int n;
-
-    if(scanf("%d",&n)!=1)
-        return(NULL);
-
-    status = pthread_create(&thread, NULL, tfun, NULL);
-    if (status != 0) {
-        printf("main error: can't create thread, status = %d\n", status);
-        exit(-11);
-    }
- 
-    status = pthread_join(thread, (void**)&status_addr);
-    if (status) {
-        printf("main error: can't join thread, status = %d\n", status);
-        exit(-12);
-    }
-
-    printf("%d\n",n);
     return(NULL);
 }
 
 int main(int argc, char** argv) {
-    args_t args;
-    args.n=0;
-    tfun(&args);
+    pthread_t *threads;
+    int status;
+    int status_addr;
+
+    int k,n;
+    if(argc!=2 || sscanf(argv[1],"%d",&n)!=1) {
+        fprintf(stderr,"%s 3\n",argv[0]);
+        return(1);
+    }
+    threads=malloc(n*sizeof(pthread_t));
+    if(threads==NULL) {
+        perror("malloc");
+        exit(-13);
+    }
+#if DEBUG
+    printf("n=%d\n",n);
+#endif    
+    for(k=0; k<n; k++) {
+        status = pthread_create(&threads[k], NULL, tfun, NULL);
+        if (status != 0) {
+            printf("main error: can't create thread, status = %d\n", status);
+            exit(-11);
+        }
+    }
+    for(k=0; k<n; k++) {
+        status = pthread_join(threads[k], (void**)&status_addr);
+        if (status) {
+            printf("main error: can't join thread, status = %d\n", status);
+            exit(-12);
+        }
+    }
+    printf("%d\n",n);
+    free(threads);
     return(0);
 }
