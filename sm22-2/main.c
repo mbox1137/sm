60edf99b2d788674f5cd92a4c0dddab38d3f40e8
sezentt <drunchic12@mail.ru>
2020-06-08 20:10:15 +0300

    sm22-2:_

--- sm22-2/main.c
+++ sm22-2/main.c
@@ -1,40 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-//pthread_create pthread_join
 
-typedef struct {
-    int n;
-} args_t;
+typedef struct
+{
+    int num;
+} Argst;
 
-void* tfun(void *args) {
+void* tfun(void *args)
+{
     pthread_t thread;
     int status;
     int status_addr;
     int n;
 
-    if(scanf("%d",&n)!=1)
+    if (scanf("%d", &n) != 1)
         return(NULL);
 
     status = pthread_create(&thread, NULL, tfun, NULL);
-    if (status != 0) {
+
+    if (status != 0)
+    {
         printf("main error: can't create thread, status = %d\n", status);
-        exit(-11);
-    }
- 
-    status = pthread_join(thread, (void**)&status_addr);
-    if (status) {
-        printf("main error: can't join thread, status = %d\n", status);
-        exit(-12);
+        exit(-1);
     }
 
-    printf("%d\n",n);
+    status = pthread_join(thread, (void**)&status_addr);
+
+    if (status)
+    {
+        printf("main error: can't join thread, status = %d\n", status);
+        exit(-1);
+    }
+
+    printf("%d\n", n);
+
     return(NULL);
 }
 
-int main(int argc, char** argv) {
-    args_t args;
-    args.n=0;
+int main(int argc, char* argv[])
+{
+    Argst args;
     tfun(&args);
-    return(0);
+
+    return 0;
 }
