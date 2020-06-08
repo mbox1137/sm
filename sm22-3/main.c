61fedfdd28928c7143892e4ee670ee0e94d034e1
sezentt <drunchic12@mail.ru>
2020-06-08 20:53:28 +0300

    sm22-3:_

--- sm22-3/main.c
+++ sm22-3/main.c
@@ -1,72 +1,98 @@
-//https://sodocumentation.net/ru/pthreads
-
 #define DEBUG 0
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <sched.h>
-//pthread_create pthread_join
 
-typedef struct {
-    int n;
-} args_t;
+typedef struct
+{
+    long long n;
+} Argst;
 
-void* tfun(void *args) {
-    int sum,num;
-    args_t *a;
-    a=malloc(sizeof(a));
-    if(a==NULL) {
+void* tfun(void *args)
+{
+    int num;
+    long long sum;
+
+    Argst *a;
+    a = malloc(sizeof(Argst));
+
+    if (a == NULL)
+    {
         perror("malloc");
-        exit(-14);
+        exit(-1);
     }
-    sum=0;
-    while(scanf("%d", &num)==1) {
-        sum+=num;
+
+    sum = 0;
+    while(scanf("%d", &num) == 1)
+    {
+        sum += num;
         sched_yield();
     }
-    a->n=sum;
+
+    a->n = sum;
     return(a);
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char* argv[])
+{
     pthread_t *threads;
     int status;
     int k,n;
-    int sum;
-    args_t *pa;
+    long long sum;
+    Argst *pa;
     void *out_void;
 
-    if(argc!=2 || sscanf(argv[1],"%d",&n)!=1) {
-        fprintf(stderr,"%s 3\n",argv[0]);
+    if (argc != 2 || sscanf(argv[1], "%d", &n) != 1)
+    {
+        fprintf(stderr, "%s 3\n", argv[0]);
         return(1);
     }
-    threads=malloc(n*sizeof(pthread_t));
-    if(threads==NULL) {
+
+    threads = malloc(n * sizeof(pthread_t));
+
+    if (threads == NULL)
+    {
         perror("malloc");
-        exit(-13);
+        exit(-1);
     }
+
 #if DEBUG
-    printf("n=%d\n",n);
-#endif    
-    for(k=0; k<n; k++) {
+    printf("n=%d\n", n);
+#endif
+
+    for(k = 0; k < n; k++)
+    {
         status = pthread_create(&threads[k], NULL, tfun, NULL);
-        if (status != 0) {
+        if (status != 0)
+        {
             printf("main error: can't create thread, status = %d\n", status);
-            exit(-11);
+            exit(-1);
         }
     }
-    sum=0;
-    for(k=0; k<n; k++) {
+
+    sum = 0;
+    for(k = 0; k < n; k++)
+    {
         status = pthread_join(threads[k], &out_void);
-        if (status) {
+        if (status)
+        {
             printf("main error: can't join thread, status = %d\n", status);
-            exit(-12);
+            exit(-1);
         }
-        pa=out_void;
-        sum+=pa->n;
+
+        pa = out_void;
+        sum += pa->n;
+        free(out_void);
     }
-    printf("%d\n",sum);
+
+    printf("%lld\n", sum);
+
     free(threads);
-    return(0);
+
+    return 0;
 }
+
+// https://sodocumentation.net/ru/pthreads
+
