a41123911d055d63238b2f23fc06bf63da5eebc3
sezentt <drunchic12@mail.ru>
2020-06-10 22:21:13 +0300

    sm23-1: done

--- sm23-1/main.c
+++ sm23-1/main.c
@@ -1,86 +1,109 @@
-//https://sodocumentation.net/ru/pthreads
-//https://learnc.info/c/pthreads_mutex_introduction.html
-
 #define DEBUG 0
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-//#include <sched.h>
 #include <sys/eventfd.h>
 #include <unistd.h>
-//#include <sys/select.h>
-#include <poll.h>
-//pthread_create pthread_join
 
-#define nn 3
-#define ni 1000000
+#define NN 3
+#define NI 1000000
 
-static double x[nn];
+static double x[NN];
 
-typedef struct args_tag {
+typedef struct Argstag
+{
     int n;
     pthread_mutex_t *pmutex;
-} args_t;
+} Argst;
 
-void* tfun(void *args) {
-    args_t *a=args;
+void* tfun(void *args)
+{
+    Argst *a = args;
     int i, k;
-    k=a->n;
-    for(i=0; i<ni; i++) {
-        pthread_mutex_lock(  (((args_t*)args)->pmutex));
-        x[k]+=(k+1)*100;
-        x[(k+1)%nn]-=(k+1)*100+1;
-        pthread_mutex_unlock((((args_t*)args)->pmutex));
+    k = a->n;
+
+    for(i = 0; i < NI; i++)
+    {
+        pthread_mutex_lock(((Argst*)args)->pmutex);
+        x[k] += (k + 1) * 100;
+        x[(k + 1) % NN] -= (k + 1) * 100 + 1;
+        pthread_mutex_unlock(((Argst*)args)->pmutex);
     }
+
     return(args);
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char* argv[])
+{
     int k;
     int status;
-    args_t *ums;
+    Argst *ums;
     pthread_t *threads;
     pthread_mutex_t mutex;
     void* result;
 
-    for(k=0; k<nn; k++)
-        x[k]=0.0;
+    for(k = 0; k < NN; k++)
+        x[k] = 0.0;
 
-    threads=malloc(nn*sizeof(pthread_t));
-    if(threads==NULL) {
+    threads = malloc(NN * sizeof(pthread_t));
+
+    if (threads == NULL)
+    {
         perror("malloc threads");
-        exit(-13);
+        exit(-1);
     }
-    ums=malloc(nn*sizeof(args_t));
-    if(ums==NULL) {
+
+    ums = malloc(NN * sizeof(Argst));
+
+    if (ums == NULL)
+    {
         perror("malloc args");
-        exit(-12);
+        exit(-1);
     }
-    pthread_mutex_init(&mutex, NULL);
-    for(k=0; k<nn; k++) {
-        ums[k].n=k;
-        ums[k].pmutex=&mutex;
+
+    if (pthread_mutex_init(&mutex, NULL) != 0)
+        exit(1);
+
+    for(k = 0; k < NN; k++)
+    {
+        ums[k].n = k;
+        ums[k].pmutex = &mutex;
         status = pthread_create(&threads[k], NULL, tfun, &ums[k]);
-        if (status != 0) {
+
+        if (status != 0)
+        {
             printf("main error: can't create thread, status = %d\n", status);
-            exit(-11);
+            exit(-1);
         }
     }
+
 #if DEBUG
-    printf("started...\n",nn);
-#endif    
-    for(k=0; k<nn; k++) {
+    printf("started...\n", NN);
+#endif
+
+    for(k = 0; k < NN; k++)
+    {
         status = pthread_join(threads[k], &result);
-        if (status) {
+
+        if (status)
+        {
             printf("main error: can't join thread, status = %d\n", status);
-            exit(-12);
+            exit(-1);
         }
     }
+
     pthread_mutex_destroy(&mutex);
+
     free(ums);
     free(threads);
-    for(k=0; k<nn; k++)
+
+    for(k = 0; k < NN; k++)
         printf("%.10lg\n", x[k]);
-    return(0);
+
+    return 0;
 }
+
+// https://sodocumentation.net/ru/pthreads
+// https://learnc.info/c/pthreads_mutex_introduction.html
+
