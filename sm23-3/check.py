e3f164d67bee009354f52bcac5a2e5cb942e147b
Alex B <alexandr.belonozhkin@gmail.com>
2020-06-11 20:37:40 +0300

    sm23-3: !

--- sm23-3/check.py
+++ sm23-3/check.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python
+
+import sys
+
+vals=dict()
+NN=100
+NI=1000
+for k in range(NN*NI):
+    vals[k]=0
+while True:
+    str=sys.stdin.readline()
+    if(len(str)==0):
+        break;
+    vals[int(str)]+=1;
+for val, count in vals.items():
+    if count==1:
+        continue
+    print(val, count)
--- sm23-3/main.sh
+++ sm23-3/main.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo 1 2 4 3 5 6 9 2 |./main 3
+./main |./check.py
