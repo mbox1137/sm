4ad937e9b68361eb7a0eff9b77f93e2a343e97bc
Alex B <alexandr.belonozhkin@gmail.com>
2020-06-13 09:29:08 +0300

    sm22-4: _

--- sm22-4/main.sh
+++ sm22-4/main.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-echo 1 2 4 3 5 6 9 2 |./main 3
-echo
-echo -1 2 4 -3 5  6 -9 2 |./main 3
+function myrun
+{
+	cmd="echo $2 |./main $1"
+	echo $cmd
+	bash -c "$cmd"
+	cmd="echo $2 |./main_poll_efd $1"
+	echo $cmd
+	bash -c "$cmd"
+	echo --------------------------------
+}
+
+myrun 3 "1 2 4 3 5 6 9 2"
+myrun 3 "-1 2 4 -3 5  6 -9 2"
