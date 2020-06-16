3ed8d5396c15555c891ebfff8830a420396e6a92
Alex B <alexandr.belonozhkin@gmail.com>
2020-06-16 22:33:06 +0300

    sm21-4: _

--- sm21-4/run.h
+++
@@ -1,7 +0,0 @@
-int run(const char* cmd, 
-        const char* input, 
-        char** poutput, 
-        char** perror, 
-        int timeot);
-char* readpipe(int fd);
-void writepipe(int fd, const char* s, int n);
