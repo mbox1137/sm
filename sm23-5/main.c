42072912798cbe97555e98665b34a4536e689dd8
Alex B <alexandr.belonozhkin@gmail.com>
2020-06-20 21:37:37 +0300

    sm23-5: _

--- sm23-5/main.c
+++ sm23-5/main.c
@@ -1,166 +1,116 @@
 //Задача о читателях-писателях: https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B4%D0%B0%D1%87%D0%B0_%D0%BE_%D1%87%D0%B8%D1%82%D0%B0%D1%82%D0%B5%D0%BB%D1%8F%D1%85-%D0%BF%D0%B8%D1%81%D0%B0%D1%82%D0%B5%D0%BB%D1%8F%D1%85
-//https://ru.wikipedia.org/wiki/%D0%A3%D1%81%D0%BB%D0%BE%D0%B2%D0%BD%D0%B0%D1%8F_%D0%BF%D0%B5%D1%80%D0%B5%D0%BC%D0%B5%D0%BD%D0%BD%D0%B0%D1%8F
 
 #define DEBUG 0
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <pthread.h>
-#include <sys/eventfd.h>
+#include <stdio.h>
 #include <unistd.h>
-#include <limits.h>
+#include <math.h>
+#include <stdlib.h>
+#include <semaphore.h>
+#define M 4 //num of WR
+#define N 3 //num of RE
 
-typedef struct
+struct FairRwLock
 {
-    double sum;
-    int locked;
-} Acct;
+	int i;
+};
 
-typedef struct Argstag
+typedef struct FairRwLock fair_rwlock_t;
+
+#define FAIR_RWLOCK_INITIALIZER {0}
+
+fair_rwlock_t myrw=FAIR_RWLOCK_INITIALIZER;
+
+void fair_rwlock_init(fair_rwlock_t *prw);
+void fair_rwlock_destroy(fair_rwlock_t *prw);
+void fair_rwlock_wrlock(fair_rwlock_t *prw);
+void fair_rwlock_rdlock(fair_rwlock_t *prw);
+void fair_rwlock_unlock(fair_rwlock_t *prw);
+
+//----------------Рыба
+
+unsigned int iter; //iteration
+sem_t accessM,readresM,orderM; //sem.
+unsigned int readers = 0;	// Number of readers accessing the resource
+
+void *reader(void *prm)
 {
-    pthread_mutex_t *pmutex;
-    pthread_cond_t *cond;
-    Acct *accs;
-    int num;
-    int iters;
-    int acc1;
-    double ds1;
-    int acc2;
-    double ds2;
-} Argst;
+	int num1=*(int*)prm;
+	int i=0,r;
+	for(i;i<iter;i++)
+	{
+		
+		if (sem_wait(&orderM)==0) printf("%d Читатель %d в очереди__________Ч%d\n",i,num1,num1);	// Remember our order of arrival
+		sem_wait(&readresM);				 // We will manipulate the readers counter
+		if (readers == 0)				// If there are currently no readers (we came first)...
+			sem_wait(&accessM);				// ...requests exclusive access to the resource for readers
+		readers++;							 // Note that there is now one more reader
+		sem_post(&orderM);					 // Release order of arrival semaphore (we have been served)
+		sem_post(&readresM);				 // We are done accessing the number of readers for now
 
-void* tfun(void *args)
-{
-    Argst *a = args;
-    int i;
-
-    for(i = 0; i < a->iters; i++)
-    {
-        pthread_mutex_lock(a->pmutex);
-        while(a->accs[a->acc1].locked || a->accs[a->acc2].locked)
-        {
-            pthread_cond_wait(a->cond, a->pmutex);
-        }
-        a->accs[a->acc1].locked = 1;
-        a->accs[a->acc2].locked = 1;
-        pthread_mutex_unlock(a->pmutex);
-
-        a->accs[a->acc1].sum += a->ds1;
-        a->accs[a->acc2].sum += a->ds2;
-        a->accs[a->acc1].locked = 0;
-        a->accs[a->acc2].locked = 0;
-        pthread_cond_signal(a->cond);
-    }
-
-    return(args);
+		printf("%d Работает читатель %d________________Ч%d\n",i,num1,num1);				// Here the reader can read the resource at will
+		r=1+rand()%4;
+		sleep(r);
+		sem_wait(&readresM);				 // We will manipulate the readers counter
+		readers--;							 // We are leaving, there is one less reader
+		if (readers == 0)				// If there are no more readers currently reading...
+			sem_post(&accessM);				// ...release exclusive access to the resource
+		sem_post(&readresM);				 // We are done accessing the number of readers for now
+	}
+	return(NULL);
 }
 
-int main(int argc, char* argv[])
+void *writer(void *prm)
 {
-    int k;
-    int status;
-    Argst *ums;
-    Acct *accs;
-    pthread_t *threads;
-    pthread_mutex_t mutex;
-    pthread_cond_t condition;
-    void* result;
-    int acc_count, thr_count;
+	int num2=*(int*)prm;
+	int j=0,r;
+	for(j;j<iter;j++)
+	{
+		if(sem_wait(&orderM)==0) printf("%d Писатель %d в очереди__________П%d\n",j,num2,num2); // Remember our order of arrival
+		sem_wait(&accessM);					// Request exclusive access to the resource
+		sem_post(&orderM);					 // Release order of arrival semaphore (we have been served)
 
-    if (scanf("%d%d", &acc_count, &thr_count) != 2)
-        exit(-1);
-
-    threads = malloc(thr_count * sizeof(pthread_t));
-    if (threads == NULL)
-    {
-        perror("malloc threads");
-        exit(-1);
-    }
-
-    ums = (Argst*)malloc(thr_count * sizeof(Argst));
-    if (ums == NULL)
-    {
-        perror("malloc args");
-        exit(-1);
-    }
-
-    accs = (Acct*)malloc(acc_count * sizeof(Acct));
-    if (accs == NULL)
-    {
-        perror("malloc accs");
-        exit(-1);
-    }
-
-    for(k = 0; k < thr_count; k++)
-        if(scanf("%d%d%lg%d%lg", &ums[k].iters, &ums[k].acc1, &ums[k].ds1, &ums[k].acc2, &ums[k].ds2) != 5)
-            exit(-1);
-
-#if DEBUG
-    printf("acc_count=%d\n", acc_count);
-    printf("thr_count=%d\n", thr_count);
-    for(k = 0; k < thr_count; k++)
-        printf("iters=%d acc1=%d ds1=%lg acc2=%d ds2=%lg\n",
-        ums[k].iters, ums[k].acc1, ums[k].ds1, ums[k].acc2, ums[k].ds2);
-#endif
-
-    for(k = 0; k < acc_count; k++)
-    {
-        accs[k].sum = 0.0;
-        accs[k].locked = 0;
-    }
-
-    if (pthread_mutex_init(&mutex, NULL) != 0)
-        exit(1);
-
-    if(pthread_cond_init(&condition,0) !=0)
-        exit(1);
-
-    pthread_attr_t attr;
-    pthread_attr_init(&attr);
-    pthread_attr_setstacksize(&attr, PTHREAD_STACK_MIN);
-
-    for(k = 0; k < thr_count; k++)
-    {
-        ums[k].num = k;
-        ums[k].pmutex = &mutex;
-        ums[k].cond = &condition;
-        ums[k].accs = accs;
-
-        status = pthread_create(&threads[k], &attr, tfun, &ums[k]);
-
-        if (status != 0)
-        {
-            printf("main error: can't create thread, status = %d\n", status);
-            exit(-1);
-        }
-    }
-
-#if DEBUG
-    printf("started...\n");
-#endif
-
-    for(k = 0; k < thr_count; k++)
-    {
-        status = pthread_join(threads[k], &result);
-
-        if (status)
-        {
-            printf("main error: can't join thread, status = %d\n", status);
-            exit(-1);
-        }
-    }
-
-    for(k = 0; k < acc_count; k++)
-        printf("%.10lg\n", accs[k].sum);
-
-    pthread_cond_destroy(&condition);
-    pthread_mutex_destroy(&mutex);
-    free(accs);
-    free(ums);
-    free(threads);
-
-    return 0;
+		printf("%d Работает писатель %d________________П%d\n",j,num2,num2);				 // Here the writer can modify the resource at will
+		r=1+rand()%4;
+		sleep(r);
+		sem_post(&accessM);					// Release exclusive access to the resource
+	}
+	return(NULL);
 }
 
-// https://sodocumentation.net/ru/pthreads
-// https://learnc.info/c/pthreads_mutex_introduction.html
+void main()
+{	
+	pthread_t threadRE[N];
+	pthread_t threadWR[M];
+	sem_init(&accessM,0,1);
+	sem_init(&readresM,0,1);
+	sem_init(&orderM,0,1);
+
+	printf("Введите количество итераций: ");
+	scanf("%d",&iter);
+	printf("Iter                         ОЧЕРЕДЬ/ВЫПОЛНЕНИЕ\n");
+	int i;
+	for(i=0;i<M;i++)
+	{
+		pthread_create(&(threadWR[i]),NULL,writer,(void*)&i);
+	}
+	for(i=0;i<N;i++)
+	{
+		pthread_create(&(threadRE[i]),NULL,reader,(void*)&i);
+	}
+	
+
+	for(i=0;i<N;i++)
+	{
+		pthread_join(threadRE[i],NULL);
+	}
+	for(i=0;i<M;i++)
+	{
+		pthread_join(threadWR[i],NULL);
+	}
+	
+	sem_destroy(&accessM);
+	sem_destroy(&readresM);
+	sem_destroy(&orderM);
+}
