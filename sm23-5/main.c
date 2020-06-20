25acb85e523edd8fd4597383ee75925e04c5a4f3
Alex B <alexandr.belonozhkin@gmail.com>
2020-06-20 20:59:31 +0300

    sm23-5: _

--- sm23-5/main.c
+++ sm23-5/main.c
@@ -1,3 +1,4 @@
+//Задача о читателях-писателях: https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B4%D0%B0%D1%87%D0%B0_%D0%BE_%D1%87%D0%B8%D1%82%D0%B0%D1%82%D0%B5%D0%BB%D1%8F%D1%85-%D0%BF%D0%B8%D1%81%D0%B0%D1%82%D0%B5%D0%BB%D1%8F%D1%85
 //https://ru.wikipedia.org/wiki/%D0%A3%D1%81%D0%BB%D0%BE%D0%B2%D0%BD%D0%B0%D1%8F_%D0%BF%D0%B5%D1%80%D0%B5%D0%BC%D0%B5%D0%BD%D0%BD%D0%B0%D1%8F
 
 #define DEBUG 0
