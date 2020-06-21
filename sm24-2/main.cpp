4be3e4a5049c6b685cfdd9df34082f17c692d42a
sezentt <drunchic12@mail.ru>
2020-06-21 19:39:26 +0300

    sm24-2:_

--- sm24-2/main.cpp
+++ sm24-2/main.cpp
@@ -11,12 +11,15 @@ int if_prime(uint64_t n)
     uint64_t i;
     if (n > 1)
     {
-        for(i = 2; i * i <= n; ++i)
+        if (n == 2)
+            return 1;
+
+        for(i = 3; i * i <= n; i += 2)
         {
             if ((n % i) == 0)
                 return 0;
         }
-        return 1;
+        return (n % 2);
     }
 
     return 0;
